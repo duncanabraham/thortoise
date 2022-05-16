a11f7475fb2721f23510b6104697266fd4d53db3
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-05-16 09:12:05 +0100

    added hardware connection

--- servo.demo.js
+++ servo.demo.js
@@ -5,10 +5,12 @@ const Triplet = require('./lib/triplet')
 
 const driver = new ServoDriver()
 
+const ms = 1000
+
 // Define the servos to form leg0
-const servo0 = new Servo({ id: 0, name: 'hip0', debug: false }, driver)
-const servo1 = new Servo({ id: 1, name: 'femur0', debug: true }, driver)
-const servo2 = new Servo({ id: 2, name: 'knee0', debug: false }, driver)
+const servo0 = new Servo({ id: 0, name: 'hip0', debug: false, ms }, driver)
+const servo1 = new Servo({ id: 1, name: 'femur0', debug: true, ms }, driver)
+const servo2 = new Servo({ id: 2, name: 'knee0', debug: false, ms }, driver)
 
 // Define the servos to form leg1
 // const servo3 = new Servo({ id: 3, name: 'hip1', debug: true }, driver)
@@ -36,26 +38,14 @@ const leg0 = new Leg({ hip: servo0, femur: servo1, knee: servo2 })
 // console.log(leg2)
 // console.log(leg3)
 
-const pos0 = new Triplet(0, 1, 2)
-const pos1 = new Triplet(1, 2, 1)
-const pos2 = new Triplet(2, 0, 0)
+const pos0 = new Triplet(0, 0, 0)
+const pos1 = new Triplet(1, 1, 1)
+const pos2 = new Triplet(2, 2, 2)
 const pos3 = new Triplet(1, 1, 1)
-const pos4 = new Triplet(0, 2, 2)
+const pos4 = new Triplet(2, 2, 2)
+const pos5 = new Triplet(1, 1, 1)
 
-const walk0 = [pos0, pos1, pos2, pos3, pos4, pos3, pos2, pos1]
-// const walk1 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
-// const walk2 = [pos2, pos3, pos4, pos3, pos2, pos1, pos0, pos1]
-// const walk3 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
-
-const left0 = [pos1, pos1, pos1, pos1, pos1, pos1, pos1, pos1]
-const left1 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
-const left2 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
-const left3 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
-
-const right0 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
-const right1 = [pos1, pos1, pos1, pos1, pos1, pos1, pos1, pos1]
-const right2 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
-const right3 = [pos1, pos2, pos3, pos4, pos3, pos2, pos1, pos0]
+const walk0 = [pos0, pos1, pos2, pos3, pos4, pos5]
 
 
 // console.log(walk0)
@@ -79,5 +69,5 @@ const walking = () => {
   }
 }
 
-const walkingLoop = setInterval(walking, 1000) // Loop forever, run the walking routine every second
+const walkingLoop = setInterval(walking, ms) // Loop forever, run the walking routine every second
 
