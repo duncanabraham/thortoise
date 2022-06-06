3048531cd0e2999f4880f7375017db30c2af047a
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-06 13:38:30 +0100

    added some test code for the GPS module

---
+++ trials/gps.js
@@ -0,0 +1,27 @@
+const SerialPort = require('serialport')
+const SerialPortParser = require('@serialport/parser-readline')
+const GPS = require('gps')
+
+const port = new SerialPort('/dev/ttyS0', { baudRate: 9600 })
+const gps = new GPS()
+
+const parser = port.pipe(new SerialPortParser())
+
+gps.on('data', async data => {
+  if(data.type == 'GGA') {
+      if(data.quality != null) {
+          let address = await getAddressInformation(data.lat, data.lon)
+          console.log(address.Label + ' [' + data.lat + ', ' + data.lon + ']')
+      } else {
+          console.log('no gps fix available')
+      }
+  }
+})
+
+parser.on('data', data => {
+  try {
+      gps.update(data)
+  } catch (e) {
+      throw e
+  }
+})
