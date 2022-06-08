51f597116a47c600ced5be417642f05acec3f34d
Duncan Abraham <dunc@buzzler.co.uk>
2022-06-08 16:07:54 +0100

    added appmap

--- tests/leg.test.js
+++ tests/leg.test.js
@@ -40,6 +40,29 @@ describe('The Leg class: ', () => {
     const servos = Object.keys(leg.servos)
     expect(servos.length).to.equal(3)
   })
+  describe('setDirection()', () => {
+    it('should allow the direction to be set', () => {
+      leg.setDirection('testdirection')
+      expect(leg.direction).to.equal('testdirection')
+    })
+  })
+  describe('getDirection()', () => {
+    describe('after instantiation', () => {
+      it('should get the direction which will be undefined', () => {
+        const direction = leg.getDirection()
+        expect(direction).to.equal(undefined)
+      })
+    })
+    describe('after being set to a value', () => {
+      it('should return the value that has been set', () => {
+        const setValue = 'FORWARD'
+        leg.setDirection(setValue)
+        const direction = leg.getDirection()
+        expect(direction).to.equal(setValue)
+      })
+    })
+  })
+
   describe('the servos:', () => {
     it('should have a pin number based on the leg id', () => {
       expect(leg.servos.hip.pin).to.equal(leg.baseId + 0)
@@ -56,6 +79,97 @@ describe('The Leg class: ', () => {
       const expectedPosition = new Triplet(45, 45, 45)
       leg.home()
       expect(leg.position.equals(expectedPosition)).to.equal(true)
+    })
+    describe('min()', () => {
+      let calls = {}
+      function mockMin() {
+        calls[this.name] = {
+          minCalled: true
+        }
+      }
+      beforeEach(() => {
+        Object.keys(leg.servos).forEach(servo => {
+          leg.servos[servo].min = mockMin.bind(leg.servos[servo])
+        })
+      })
+      it('should call the min function on each servo', () => {
+        leg.min()
+        const expectedResult = {
+          hip9: { minCalled: true },
+          femur9: { minCalled: true },
+          knee9: { minCalled: true }
+        }
+        expect(calls).to.deep.equal(expectedResult)
+      })
+    })
+    describe('max()', () => {
+      let calls = {}
+      function mockMax() {
+        calls[this.name] = {
+          maxCalled: true
+        }
+      }
+      beforeEach(() => {
+        Object.keys(leg.servos).forEach(servo => {
+          leg.servos[servo].max = mockMax.bind(leg.servos[servo])
+        })
+      })
+      it('should call the min function on each servo', () => {
+        leg.max()
+        const expectedResult = {
+          hip9: { maxCalled: true },
+          femur9: { maxCalled: true },
+          knee9: { maxCalled: true }
+        }
+        expect(calls).to.deep.equal(expectedResult)
+      })
+    })
+    describe('directionFromStep()', () => {
+      describe('when step is NOT set', () => {
+        it('should set step to the value passed in', () => {
+          leg.directionFromStep(2)
+          expect(leg.step).to.equal(2)
+        })
+      })
+      describe('when step is set', () => {
+        beforeEach(() => {
+          leg.step = 10
+        })
+        it('should set use the existing value', () => {
+          leg.directionFromStep(2)
+          expect(leg.step).to.equal(10)
+        })
+      })
+      describe('when the direction is "forward"', () => {
+        beforeEach(() => {
+          leg.setDirection('forward')
+        })
+        it('should increment the step by 1', () => {
+          leg.directionFromStep(2)
+          expect(leg.step).to.equal(3)
+        })
+        describe('and the forward value exceeds the maximum steps', () => {
+          it('should set the step to 0', () => {
+            leg.directionFromStep(leg.steps)
+            expect(leg.step).to.equal(0)
+          })
+        })
+      })
+      describe('when the direction is "backward"', () => {
+        beforeEach(() => {
+          leg.setDirection('backward')
+        })
+        it('should decrement the step by 1', () => {
+          leg.directionFromStep(2)
+          expect(leg.step).to.equal(1)
+        })
+        describe('and the step count is less than zero', () => {
+          it('should set the step count to the maximum step value', () => {
+            leg.directionFromStep(0)
+            expect(leg.step).to.equal(leg.steps - 1)
+          })
+        })
+      })
     })
   })
 })
