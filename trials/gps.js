ff62070b3605fb50c84bc6454a77e71f07c2d7d9
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-13 13:54:32 +0100

    removed unnecessary python scripts and updated gps.js ready to test with new module

--- trials/gps.js
+++ trials/gps.js
@@ -2,26 +2,21 @@ const SerialPort = require('serialport').SerialPort
 const SerialPortParser = require('@serialport/parser-readline')
 const GPS = require('gps')
 
-const port = new SerialPort({ path: '/dev/ttyAMA0', baudRate: 9600 })
+const port = new SerialPort({ path: '/dev/ttySC0', baudRate: 9600 })
 const gps = new GPS()
 
 const parser = port.pipe(new SerialPortParser())
 
-gps.on('data', async data => {
-  if (data.type == 'GGA') {
+gps.on('data', async data => { // when we get sent data from the port monitor, output our location
+  if (data.type === 'GGA') {
     if (data.quality != null) {
-      let address = await getAddressInformation(data.lat, data.lon)
-      console.log(address.Label + ' [' + data.lat + ', ' + data.lon + ']')
+      console.log(`you're at: ${data.lat}, ${data.lon}`)
     } else {
       console.log('no gps fix available')
     }
   }
 })
 
-parser.on('data', data => {
-  try {
-    gps.update(data)
-  } catch (e) {
-    throw e
-  }
+parser.on('data', data => { // when we see data on the serial port, pass it to the gps module
+  gps.update(data)
 })
--- trials/hattest.py
+++
@@ -1,6 +0,0 @@
-from gsmHat import GSMHat, SMS, GPS
-gsm = GSMHat('/dev/serial0', 115200)
-
-Number = 'xxxxxxxxxx'
-Message = 'just work now, please'
-gsm.SMS_write(Number, Message)
