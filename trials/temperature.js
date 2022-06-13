da3c5a7e733033141e1e3943b5f4e58651ae006f
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-13 14:29:07 +0100

    temperature works

--- trials/temperature.js
+++ trials/temperature.js
@@ -4,7 +4,7 @@ const LPS22HB_ADDR = 0x5C
 const TEMP_REG = 0x2B
 
 const toCelsius = rawData => {
-  console.log('rawData: ', rawData)
+  console.log('rawData: ', rawData,'  First 2 numbers . next 2 numbers ie 2145 = 21.45C')
   rawData = (rawData >> 8) + ((rawData & 0xff) << 8)
   let celsius = (rawData & 0x0fff) / 16
   if (rawData & 0x1000) {
