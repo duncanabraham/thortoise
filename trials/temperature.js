32c1ec6ae81b9ba5cf8580307ae26fd59d610c9e
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-13 14:36:13 +0100

    read pressure test

--- trials/temperature.js
+++ trials/temperature.js
@@ -3,6 +3,10 @@ const i2c = require('i2c-bus')
 const LPS22HB_ADDR = 0x5C
 const TEMP_REG = 0x2B
 
+const PressureRegXL = 0x28
+const PressureRegL = 0x29
+const PressureRegH = 0x2A
+
 const toCelsius = rawData => {
   console.log('rawData: ', rawData,'  First 2 numbers . next 2 numbers ie 2145 = 21.45C')
   rawData = (rawData >> 8) + ((rawData & 0xff) << 8)
@@ -13,7 +17,15 @@ const toCelsius = rawData => {
   return celsius
 }
 
+const readPressure = (i2c1) => {
+  const xl = i2c1.readByteSync(LPS22HB_ADDR, PressureRegXL)
+  const l = i2c1.readByteSync(LPS22HB_ADDR, PressureRegL)
+  const h = i2c1.readByteSync(LPS22HB_ADDR, PressureRegH)
+  console.log(`xl: ${xl}, l: ${l}, h: ${h}`)
+}
+
 const i2c1 = i2c.openSync(1)
 const rawData = i2c1.readWordSync(LPS22HB_ADDR, TEMP_REG)
+readPressure(i2c1)
 console.log(toCelsius(rawData))
 i2c1.closeSync()
