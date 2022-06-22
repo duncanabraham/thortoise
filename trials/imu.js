9299a9ff03d8e7b987718642960142695c37829f
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-22 10:53:43 +0100

    updated ICM20948 driver and test script

--- trials/imu.js
+++ trials/imu.js
@@ -1,19 +1,12 @@
-const ICM20948 = require('../lib/ICM20948')
+const ICM20948 = require('../lib/i2c/ICM20948')
 const { delay } = require('../lib/utils')
 
 const imu = new ICM20948()
 
 const run = async () => {
   while (true) {
-    imu.readAccelerometer()
-    await delay(100)
-    imu.readGyro()
-    await delay(100)
-    imu.readCompass()
-    await delay(100)
-    console.log('Gyro:  ', imu.Gyro)
-    console.log('Accel: ', imu.Accelerometer)
-    console.log('Compass: ', imu.Compass)
+    imu.getData()
+    console.log(imu.Gyro, '    ', imu.Accelerometer, '    ', imu.Compass, '    ', imu.Temperature)
     await delay(1000)
   }
 }
