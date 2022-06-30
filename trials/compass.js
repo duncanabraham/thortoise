693221a43e6793adc04809c9495807f280ad3604
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-30 08:59:41 +0100

    I think we're there

--- trials/compass.js
+++ trials/compass.js
@@ -2,10 +2,6 @@ const Compass = require('../lib/i2c/QMC5883L')
 
 const compass = new Compass({ i2cAddress: 0x0d })
 const { delay } = require('../lib/utils')
-const { Triplet } = require('../lib/triplet')
-
-const minVals = new Triplet(0, 0, 0)
-const maxVals = new Triplet(0, 0, 0)
 
 const fmt = (s, l, d) => {
   s = JSON.stringify(s)
@@ -17,13 +13,11 @@ const fmt = (s, l, d) => {
 
 const run = async () => {
   while (true) {
-    const b = compass.getBearing()
     const raw = compass.raw()
     const avg = compass.smoothed.avg()
-    const head = compass.heading()
-    minVals.minValues(raw)
-    maxVals.maxValues(raw)
-    console.log(fmt(b, 5, true), ' : ', fmt(head, 5, true), ' : ', fmt(raw, 35, false), ' : ', fmt(avg, 35, false), ' : ', fmt(minVals, 35, false), ' : ', fmt(maxVals, 35, false))
+    const head = compass.getHeading()
+    const cal = compass.getCalibration()
+    console.log(fmt(head, 5, true), ' : ', fmt(raw, 35, false), ' : ', fmt(avg, 35, false), ' : ', fmt(cal.calibrationMin, 35, false), ' : ', fmt(cal.calibrationMax, 35, false))
     await delay(1000)
   }
 }
