3f5b6d3171b6455a03054f56dceed1c6c3554fcf
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-30 09:04:26 +0100

    raw values from heading

--- trials/compass.js
+++ trials/compass.js
@@ -17,7 +17,7 @@ const run = async () => {
     const avg = compass.smoothed.avg()
     const head = compass.getHeading()
     const cal = compass.getCalibration()
-    console.log(fmt(head, 5, true), ' : ', fmt(raw, 35, false), ' : ', fmt(avg, 35, false), ' : ', fmt(cal.calibrationMin, 35, false), ' : ', fmt(cal.calibrationMax, 35, false))
+    console.log(fmt(compass._heading, 5, true), ' : ', fmt(head, 5, true), ' : ', fmt(raw, 35, false), ' : ', fmt(avg, 35, false), ' : ', fmt(cal.calibrationMin, 35, false), ' : ', fmt(cal.calibrationMax, 35, false))
     await delay(1000)
   }
 }
