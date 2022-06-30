25f3c25ef545e9fcb8f23d484c406aed971a06c3
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-06-30 09:58:09 +0100

    updated calibration script

--- trials/compass.js
+++ trials/compass.js
@@ -12,13 +12,30 @@ const fmt = (s, l, d) => {
 }
 
 const run = async () => {
+  const direction = {
+    1: 'N',
+    2: 'E',
+    3: 'NE',
+    4: 'S',
+    5: 'ERROR',
+    6: 'SE',
+    7: 'ERROR',
+    8: 'W',
+    9: 'NW',
+    10: 'ERROR',
+    11: 'ERROR',
+    12: 'SW',
+    13: 'ERROR',
+    14: 'ERROR',
+    15: 'ERROR'
+  }
   while (true) {
     const raw = compass.raw()
     const avg = compass.smoothed.avg()
     const head = compass.getHeading()
     const cal = compass.getCalibration()
     const temp = compass.getTemperature()
-    console.log(fmt(compass._heading, 5, true), ' : ', fmt(head, 5, true), ' : ', fmt(raw, 35, false), ' : ', fmt(avg, 35, false), ' : ', fmt(cal.calibrationMin, 35, false), ' : ', fmt(cal.calibrationMax, 35, false), ' : ', fmt(temp, 35, false))
+    console.log(fmt(direction[compass._heading], 6, true), ' : ', fmt(compass._heading, 5, true), ' : ', fmt(head, 5, true), ' : ', fmt(raw, 35, false), ' : ', fmt(avg, 35, false), ' : ', fmt(cal.calibrationMin, 35, false), ' : ', fmt(cal.calibrationMax, 35, false), ' : ', fmt(temp, 35, false))
     await delay(1000)
   }
 }
