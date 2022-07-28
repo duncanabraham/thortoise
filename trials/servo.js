7832ce0ddd2fc2ff1f3cf295645b628448db1e68
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-07-28 11:37:44 +0100

    sd

--- trials/servo.js
+++ trials/servo.js
@@ -2,4 +2,6 @@ require('./common')
 const ServoController = require('../lib/ServoController')
 const servoController = new ServoController()
 const servo = servoController.addServo({ pin: 0, name: 'servo0', startAt: 0, range: [0, 180] })
-servoController.to(0, 180)
+// servoController.to(0, 180)
+
+console.log(servoController)
