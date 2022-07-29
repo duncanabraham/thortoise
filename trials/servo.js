48692783d0a0b23f1c8dcd0e243e45629697035a
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-07-29 08:51:15 +0100

    tweaking

--- trials/servo.js
+++ trials/servo.js
@@ -1,7 +1,8 @@
 require('./common')
 const ServoController = require('../lib/ServoController')
 const servoController = new ServoController()
-const servo0Options = { pin: 0, name: 'servo0', range: [60, 70], startAt: 65, sleepAt: 65, standAt: 65 }
+const servo0Options = { pin: 0, name: 'frontLeftHip', range: [60, 70], startAt: 65, sleepAt: 65, standAt: 65 }
+
 const servo = servoController.addServo(servo0Options)
 
 console.log(servoController)
