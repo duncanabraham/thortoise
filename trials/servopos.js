07859580095a7a742b081f7095b58cd5834f316c
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-08-02 10:55:25 +0100

    debug

--- trials/servopos.js
+++ trials/servopos.js
@@ -2,11 +2,13 @@ require('./common')
 const Controller = require('../lib/ServoController')
 const servoController = new Controller()
 
-const servo0Options = { name: 'frontLeftHip', range: [60, 70], startAt: 65, sleepAt: 65, standAt: 65 }
+const servo0Options = { range: [60, 70], startAt: 65, sleepAt: 65, standAt: 65 }
 
-const servo0 = servoController.addServo({...servo0Options, pin: 0})
-const servo1 = servoController.addServo({...servo0Options, pin: 1})
-const servo2 = servoController.addServo({...servo0Options, pin: 2})
+const servo0 = servoController.addServo({...servo0Options, pin: 0, name: 'frontLeftHip'})
+const servo1 = servoController.addServo({...servo0Options, pin: 1, name: 'frontLeftFemur'})
+const servo2 = servoController.addServo({...servo0Options, pin: 2, name: 'frontLeftKnee'})
+
+console.log('Controller: ', servoController)
 
 console.log('servo0', servoController.getPWM(0))
 console.log('servo1', servoController.getPWM(1))
