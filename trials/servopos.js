9d3198dfebf9d9d5071e4a4d947face86c78dda1
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-08-02 11:42:12 +0100

    test

--- trials/servopos.js
+++ trials/servopos.js
@@ -4,21 +4,17 @@ const servoController = new Controller()
 
 const { delay } = require('../lib/utils')
 
-const servo0Options = { range: [10, 70], startAt: 10, sleepAt: 10, standAt: 10 }
+const servo0Options = { range: [0, 180], startAt: 90, sleepAt: 90, standAt: 90 }
 
 const servo0 = servoController.addServo({...servo0Options, pin: 0, name: 'frontLeftHip'})
-const servo1 = servoController.addServo({...servo0Options, pin: 1, name: 'frontLeftFemur'})
-const servo2 = servoController.addServo({...servo0Options, pin: 2, name: 'frontLeftKnee'})
 
 console.log('Controller: ', servoController)
 
 console.log('servo0', servoController.getPWM(0))
-console.log('servo1', servoController.getPWM(1))
-console.log('servo2', servoController.getPWM(2))
 
 servo0.to(10)
 
 delay(3000).then(()=>{
-  servo0.stop()
+  servo0.shutdown()
 })
 
