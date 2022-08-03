655a424637ada4d862ff1b0d1b5277218a941b2d
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-08-03 15:44:40 +0100

    servo reset

---
+++ trials/servoreset.js
@@ -0,0 +1,9 @@
+require('./common')
+const Controller = require('../lib/ServoController')
+const servoController = new Controller()
+const delay = require('../lib/utils').delay
+
+const servo0Options = { pin: 0, name: 'frontLeftHip', range: [10, 170], startAt: 65, sleepAt: 65, standAt: 65 }
+const servo = servoController.addServo(servo0Options)
+
+servo.stop()
