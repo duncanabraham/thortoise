f1a95cc18d4bcf0cff49b4a1f3898c273759e93d
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-08-03 08:49:57 +0100

    modified servo test script

--- trials/pyservo2.js
+++ trials/pyservo2.js
@@ -3,16 +3,20 @@ const Controller = require('../lib/ServoController')
 const servoController = new Controller()
 const delay = require('../lib/utils').delay
 
-// const servo0Options = { pin: 0, name: 'frontLeftHip', range: [10, 170], startAt: 65, sleepAt: 65, standAt: 65 }
+const servo0Options = { pin: 0, name: 'frontLeftHip', range: [10, 170], startAt: 65, sleepAt: 65, standAt: 65 }
+const servo = servoController.addServo(servo0Options)
 
-// const servo = servoController.addServo(servo0Options)
-
+// Sweep the servo from 10 to maxRange and back to 10
 const run = async () => {
-  let pulse = 10
+  let angle = 10
+  let direction = 1
+  let maxPos = servo0Options.range[1]
   while (true) {
-    servoController.setCount(0, pulse)
+    // servoController.setCount(0, pulse)
+    servo.to(angle)
+    servo
     await delay(10)
-    pulse = pulse + 1 > 170 ? 10 : pulse + 1
+    direction = angle + direction >= maxPos || angle + direction <= 10 ? -direction : direction
   }
 }
 
