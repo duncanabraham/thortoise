b583323cab86502b5c582af6c6963a8c05bf70ec
Duncan Abraham <dunc@buzzler.co.uk>
2022-08-03 07:48:51 +0100

    removed unused lines

--- trials/pyservo2.js
+++ trials/pyservo2.js
@@ -3,9 +3,9 @@ const Controller = require('../lib/ServoController')
 const servoController = new Controller()
 const delay = require('../lib/utils').delay
 
-const servo0Options = { pin: 0, name: 'frontLeftHip', range: [10, 170], startAt: 65, sleepAt: 65, standAt: 65 }
+// const servo0Options = { pin: 0, name: 'frontLeftHip', range: [10, 170], startAt: 65, sleepAt: 65, standAt: 65 }
 
-const servo = servoController.addServo(servo0Options)
+// const servo = servoController.addServo(servo0Options)
 
 const run = async () => {
   let pulse = 10
