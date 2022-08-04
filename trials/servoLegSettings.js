ff747b521d92b2aa93cf06a7a2063a3a1f6a3aa9
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-08-04 11:09:44 +0100

    debug

--- trials/servoLegSettings.js
+++ trials/servoLegSettings.js
@@ -3,23 +3,15 @@ const Controller = require('../lib/ServoController')
 const servoController = new Controller()
 const delay = require('../lib/utils').delay
 
-const config = require('../config')
-
-const servoConfig = [
-  config.servosFrontLeft,
-  config.servosFrontRight,
-  config.servosBackLeft,
-  config.servosBackRight
-]
-
-console.log('servoConfig: ', servoConfig)
+const {options:{legSettings}} = require('../config')
 
 const servoNames = ['hipServoSettings', 'femurServoSettings', 'kneeServoSettings']
 
 const testLeg = 0
 const testServo = 0
 
-const servo = servoController.addServo(servoConfig[testLeg][servoNames[testServo]])
+console.log('legSettings', legSettings[testLeg])
+const servo = servoController.addServo(legSettings[testLeg][servoNames[testServo]])
 console.log('servo: ', servo)
 
 // Sweep the servo from 10 to maxRange and back to 10
