02ba58122bed9bca8fce21bd26b4bf51eedd1267
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-08-08 14:25:15 +0100

    mpu

---
+++ trials/mpu.js
@@ -0,0 +1,11 @@
+const MPU6050 = require('../lib/i2c/MPU6050')
+const imu = new MPU6050()
+
+imu.setAccelRange(imu.ACCEL_RANGE_8G)
+imu.run()
+
+while (true) {
+  const data = imu.getValue()
+  console.log(data)
+}
+
