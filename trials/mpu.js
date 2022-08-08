fd92c87ed337b027eac757b00edd7b3171e765a0
Duncan Abraham <duncan@tydusolutions.co.uk>
2022-08-08 14:27:25 +0100

    temp

--- trials/mpu.js
+++ trials/mpu.js
@@ -5,6 +5,8 @@ const imu = new MPU6050()
 imu.setAccelRange(imu.ACCEL_RANGE_8G)
 imu.run()
 
+console.log('temperature:', imu.getTemp())
+
 while (true) {
   const data = imu.getValue()
   console.log(data)
