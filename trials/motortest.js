5338ab3f15697618399bfb1d988a609b1410f0ce
Duncan Abraham <duncan@tydusolutions.co.uk>
2023-08-23 09:18:29 +0100

    working motortest

--- trials/motortest.js
+++ trials/motortest.js
@@ -1,20 +1,28 @@
 const ODrive = require('../lib/odrive')
 const { delay } = require('../lib/utils')
 
-const motorController = new ODrive('/dev/ttyACM0', 115200)
+const motorController = new ODrive('/dev/ttyACM1', 115200)
 
-const desiredVelocity = 32 // Replace with your desired velocity in RPM
+const desiredVelocity = 16 // Replace with your desired velocity in RPM
 
 const run = async () => {
   try {
     await motorController.init()
     await motorController.calibrate()
 
-    await delay(20000)
-    await motorController.write('w axis1.requested_state 8\n')
-
+    await delay(10000)
+    
     await motorController.write('w axis1.controller.config.control_mode 2\n') // 2 corresponds to CONTROL_MODE_VELOCITY_CONTROL
+    await motorController.write('w axis1.requested_state 8\n')
+    
     await motorController.write(`w axis1.controller.input_vel ${desiredVelocity}\n`)
+
+    await delay(5000)
+    await motorController.write(`w axis1.controller.input_vel ${desiredVelocity * -1}\n`)
+
+    await delay(5000)
+    await motorController.write('w axis1.requested_state 1\n')
+
     console.log(`Set velocity of axis1 to ${desiredVelocity / 64} RPM`) // 64:1 gearbox
   } catch (error) {
     console.error('Error:', error)
