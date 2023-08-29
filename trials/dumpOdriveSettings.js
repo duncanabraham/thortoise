85d3737ee35d5b2e1b9af4d6ebd812ee1be2b24c
Duncan Abraham <duncan@tydusolutions.co.uk>
2023-08-29 08:35:41 +0100

    local changes

---
+++ trials/dumpOdriveSettings.js
@@ -0,0 +1,20 @@
+const ODrive = require('../lib/odrive')
+const motorController = new ODrive('/dev/ttyACM0', 115200)
+
+const dumpAxisSettings = async (axis) => {
+  try {
+    await motorController.init()
+
+    const settings = await motorController.read(`d axis${axis}\n`)
+    console.log(`Settings for axis${axis}:\n${settings}`)
+  } catch (error) {
+    console.error('Error:', error)
+  } finally {
+    motorController.close()
+  }
+}
+
+// Dump settings for both axis0 and axis1
+dumpAxisSettings(0)
+dumpAxisSettings(1)
+
--- trials/motortest.js
+++ trials/motortest.js
@@ -1,7 +1,7 @@
 const ODrive = require('../lib/odrive')
 const { delay } = require('../lib/utils')
 
-const motorController = new ODrive('/dev/ttyACM1', 115200)
+const motorController = new ODrive('/dev/ttyACM0', 115200)
 
 const desiredVelocity = 16 // Replace with your desired velocity in RPM
 
@@ -12,18 +12,28 @@ const run = async () => {
 
     await delay(10000)
     
+    await motorController.write('w axis0.controller.config.control_mode 2\n') // 2 corresponds to CONTROL_MODE_VELOCITY_CONTROL
     await motorController.write('w axis1.controller.config.control_mode 2\n') // 2 corresponds to CONTROL_MODE_VELOCITY_CONTROL
+
+    await motorController.write('w axis0.requested_state 8\n')
     await motorController.write('w axis1.requested_state 8\n')
     
+    await motorController.write(`w axis0.controller.input_vel ${desiredVelocity}\n`)
     await motorController.write(`w axis1.controller.input_vel ${desiredVelocity}\n`)
 
     await delay(5000)
+    await motorController.write(`w axis0.controller.input_vel ${desiredVelocity * -1}\n`)
     await motorController.write(`w axis1.controller.input_vel ${desiredVelocity * -1}\n`)
 
     await delay(5000)
+    await motorController.write('w axis0.requested_state 1\n')
     await motorController.write('w axis1.requested_state 1\n')
 
     console.log(`Set velocity of axis1 to ${desiredVelocity / 64} RPM`) // 64:1 gearbox
+
+    await motorController.zeroMotors()
+    await motorController.disableMotors()
+
   } catch (error) {
     console.error('Error:', error)
   }
