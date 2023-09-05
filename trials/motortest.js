ef2882352db279f51f8caedd4eb6081f759b2ccd
Duncan Abraham <dunc@buzzler.co.uk>
2023-09-05 17:20:24 +0100

    reduced speed and duration

--- trials/motortest.js
+++ trials/motortest.js
@@ -1,12 +1,12 @@
 const ODrive = require('../lib/odrive')
 const { delay } = require('../lib/utils')
 
-const uart= '/dev/ttyAML1'
-const baud=115200
+const uart = '/dev/ttyAML1' // /dev/ttyAML0 = Radxa UART_A, /dev/ttyAML1 = Radxa UART_B
+const baud = 115200
 
 const motorController = new ODrive(uart, baud)
 
-const desiredVelocity = 16 // Replace with your desired velocity in RPM
+const desiredVelocity = 8 // Replace with your desired velocity in RPM
 
 const run = async () => {
   try {
@@ -14,21 +14,21 @@ const run = async () => {
     await motorController.calibrate()
 
     await delay(10000)
-    
+
     await motorController.write('w axis0.controller.config.control_mode 2\n') // 2 corresponds to CONTROL_MODE_VELOCITY_CONTROL
     await motorController.write('w axis1.controller.config.control_mode 2\n') // 2 corresponds to CONTROL_MODE_VELOCITY_CONTROL
 
     await motorController.write('w axis0.requested_state 8\n')
     await motorController.write('w axis1.requested_state 8\n')
-    
+
     await motorController.write(`w axis0.controller.input_vel ${desiredVelocity}\n`)
     await motorController.write(`w axis1.controller.input_vel ${desiredVelocity * -1}\n`)
 
-    await delay(5000)
+    await delay(3000)
     await motorController.write(`w axis0.controller.input_vel ${desiredVelocity * -1}\n`)
     await motorController.write(`w axis1.controller.input_vel ${desiredVelocity}\n`)
 
-    await delay(5000)
+    await delay(3000)
     await motorController.write('w axis0.requested_state 1\n')
     await motorController.write('w axis1.requested_state 1\n')
 
@@ -36,7 +36,6 @@ const run = async () => {
 
     await motorController.zeroMotors()
     await motorController.disableMotors()
-
   } catch (error) {
     console.error('Error:', error)
   }
