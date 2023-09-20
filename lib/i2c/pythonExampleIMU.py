6a56535f56b115da07e4fd8c18270e1b5ab393c1
Duncan Abraham <dunc@buzzler.co.uk>
2023-09-20 21:52:01 +0100

    switched atan2 params

--- lib/i2c/pythonExampleIMU.py
+++ lib/i2c/pythonExampleIMU.py
@@ -388,9 +388,11 @@ if __name__ == '__main__':
     pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
     roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
     yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
+    mag   = math.atan2(Mag[1], Mag[0]) * 180/math.pi
     print("\r\n /-------------------------------------------------------------/ \r\n")
     print('\r\n Roll = %.2f , Pitch = %.2f , Yaw = %.2f\r\n'%(roll,pitch,yaw))
     print('\r\nAcceleration:  X = %d , Y = %d , Z = %d\r\n'%(Accel[0],Accel[1],Accel[2]))  
     print('\r\nGyroscope:     X = %d , Y = %d , Z = %d\r\n'%(Gyro[0],Gyro[1],Gyro[2]))
     print('\r\nMagnetic:      X = %d , Y = %d , Z = %d'%((Mag[0]),Mag[1],Mag[2]))
+    print('\r\nHeading:       %d'%(mag))
     
