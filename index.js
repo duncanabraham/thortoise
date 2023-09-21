fdf0f2d8d8eb592188ea3221fc7d53c925d91369
Duncan Abraham <duncan@tydusolutions.co.uk>
2023-09-21 14:50:38 +0100

    registered redisClient at the top end and passed it down in the options

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const store = new Store()
 const { pad, niceDate } = require('./lib/utils')
 const log = require('./lib/log')
 
+const redis = require('redis')
+const redisClient = redis.createClient()
+
 // When an error is logged display it to the console
 store.attachHandler('ERRORS', (data) => {
   log.error(data)
@@ -49,7 +52,7 @@ app.listen(api.port, () => {
 
 (async () => {
   await init()  
-  const thortoise = new Thortoise({ ...options, store })
+  const thortoise = new Thortoise({ ...options, store, redisClient })
   this.controller = new Controller({ robot: thortoise, app, store })  
 
   if (thortoise.verbose) {
