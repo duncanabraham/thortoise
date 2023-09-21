fcfd83a9129137aca362c7a71f4a906985aa21a5
Duncan Abraham <duncan@tydusolutions.co.uk>
2023-09-21 16:51:21 +0100

    better startup

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const setupRedis = async () => {
   redisClient = createClient({
     host: '127.0.0.1',
     port: 6379
-  })
-    .connect()
+  }).connect()
     .then(() => {
       // Set up some global event listeners
       redisClient.on('connect', () => { console.log('Redis client connected') })
@@ -31,66 +30,61 @@ const setupRedis = async () => {
     })
 }
 
-setupRedis().then(() => {
+const Registry = require('./lib/registry')
+global.registry = new Registry()
 
-  const Registry = require('./lib/registry')
-  global.registry = new Registry()
+const Store = require('./lib/store')
+const { options, api } = require('./config')
+const Thortoise = require('./lib/thortoise')
+const Controller = require('./lib/controller')
+const express = require('express')
+const bodyParser = require('body-parser')
+const store = new Store()
+const { pad, niceDate } = require('./lib/utils')
+const log = require('./lib/log')
 
-  const Store = require('./lib/store')
-  const { options, api } = require('./config')
-  const Thortoise = require('./lib/thortoise')
-  const Controller = require('./lib/controller')
-  const express = require('express')
-  const bodyParser = require('body-parser')
-  const store = new Store()
-  const { pad, niceDate } = require('./lib/utils')
-  const log = require('./lib/log')
+// When an error is logged display it to the console
+store.attachHandler('ERRORS', (data) => {
+  log.error(data)
+})
 
+store.attachHandler('INFO', (data) => {
+  console.info(`${pad(data.key, 10, true)} ${pad(data.value, 80, true)} ${niceDate(data.time)}`)
+})
 
+const init = async () => {
+  // TODO: do I need to initialise anything here?
+  // Each module should be calling their init() methods on instantiation
+  await setupRedis()
+}
 
-  // When an error is logged display it to the console
-  store.attachHandler('ERRORS', (data) => {
-    log.error(data)
-  })
+const app = express()
 
-  store.attachHandler('INFO', (data) => {
-    console.info(`${pad(data.key, 10, true)} ${pad(data.value, 80, true)} ${niceDate(data.time)}`)
-  })
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-  const init = async () => {
-    // TODO: do I need to initialise anything here?
-    // Each module should be calling their init() methods on instantiation
+app.listen(api.port, () => {
+  store.append('INFO', `Thortoise is listening on port ${api.port}`)
+});
+
+(async () => {
+  await init()
+  const thortoise = new Thortoise({ ...options, store, redisClient })
+  this.controller = new Controller({ robot: thortoise, app, store })
+
+  if (thortoise.verbose) {
+    console.info(thortoise)
   }
 
-  const app = express()
-
-  app.use(bodyParser.urlencoded({ extended: false }))
-  app.use(bodyParser.json())
-
-  app.listen(api.port, () => {
-    store.append('INFO', `Thortoise is listening on port ${api.port}`)
-  });
-
-  (async () => {
-    await init()
-    const thortoise = new Thortoise({ ...options, store, redisClient })
-    this.controller = new Controller({ robot: thortoise, app, store })
-
-    if (thortoise.verbose) {
-      console.info(thortoise)
-    }
-
-    log.info('Starting ...')
-    log.info()
-    log.info('████████ ██   ██  ██████  ██████  ████████  ██████  ██ ███████ ███████')
-    log.info('   ██    ██   ██ ██    ██ ██   ██    ██    ██    ██ ██ ██      ██')
-    log.info('   ██    ███████ ██    ██ ██████     ██    ██    ██ ██ ███████ █████')
-    log.info('   ██    ██   ██ ██    ██ ██   ██    ██    ██    ██ ██      ██ ██')
-    log.info('   ██    ██   ██  ██████  ██   ██    ██     ██████  ██ ███████ ███████')
-    log.info()
-    log.info(new Date().toISOString())
-    log.info()
-    thortoise.start()
-  })()
-
-})
+  log.info('Starting ...')
+  log.info()
+  log.info('████████ ██   ██  ██████  ██████  ████████  ██████  ██ ███████ ███████')
+  log.info('   ██    ██   ██ ██    ██ ██   ██    ██    ██    ██ ██ ██      ██')
+  log.info('   ██    ███████ ██    ██ ██████     ██    ██    ██ ██ ███████ █████')
+  log.info('   ██    ██   ██ ██    ██ ██   ██    ██    ██    ██ ██      ██ ██')
+  log.info('   ██    ██   ██  ██████  ██   ██    ██     ██████  ██ ███████ ███████')
+  log.info()
+  log.info(new Date().toISOString())
+  log.info()
+  thortoise.start()
+})()
