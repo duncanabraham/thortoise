5cb4195df02370dc640e74b1fd146a99ead79a61
Duncan Abraham <duncan@tydusolutions.co.uk>
2023-09-22 14:45:06 +0100

    updated mputest

--- trials/mputest.py
+++ trials/mputest.py
@@ -1,16 +1,16 @@
 import smbus
 import time
+import math
 
 bus = smbus.SMBus(1)
 address = 0x68  # default I2C address for MPU6050
 
-# Initialize MPU6050
-bus.write_byte_data(address, 0x6B, 0)
+# Initialize MPU-6050
+bus.write_byte_data(address, 0x6B, 0x00)  # Wake up MPU-6050
 
 def read_data(reg_address):
     high = bus.read_byte_data(address, reg_address)
     low = bus.read_byte_data(address, reg_address + 1)
-
     value = (high << 8) + low
 
     if value >= 0x8000:
@@ -19,14 +19,14 @@ def read_data(reg_address):
         return value
 
 while True:
-    accel_x = read_data(0x3B)
-    accel_y = read_data(0x3D)
-    accel_z = read_data(0x3F)
-    gyro_x = read_data(0x43)
-    gyro_y = read_data(0x45)
-    gyro_z = read_data(0x47)
+    accel_x = read_data(0x3B) / 16384.0
+    accel_y = read_data(0x3D) / 16384.0
+    accel_z = read_data(0x3F) / 16384.0
 
-    print(f"Accelerometer: X = {accel_x}, Y = {accel_y}, Z = {accel_z}")
-    print(f"Gyroscope: X = {gyro_x}, Y = {gyro_y}, Z = {gyro_z}")
+    # Calculate pitch and roll based on accelerometer data
+    pitch = math.atan2(accel_y, math.sqrt(accel_x**2 + accel_z**2)) * (180 / math.pi)
+    roll = math.atan2(-accel_x, accel_z) * (180 / math.pi)
 
-    time.sleep(1)
+    print(f"Pitch: {pitch:.2f}°, Roll: {roll:.2f}°")
+
+    time.sleep(0.1)
