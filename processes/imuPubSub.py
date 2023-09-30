ec2677e9c44ca1aefd121aa5e928eb7ec2f98aab
Duncan Abraham <dunc@buzzler.co.uk>
2023-09-30 10:28:03 +0100

    don't perform calculations in the raw data acquisition function

--- processes/imuPubSub.py
+++ processes/imuPubSub.py
@@ -385,13 +385,12 @@ if __name__ == '__main__':
   while True:
     icm20948.icm20948_Gyro_Accel_Read()
     icm20948.icm20948MagRead()
-    icm20948.icm20948CalAvgValue()
-    time.sleep(0.5)
-    icm20948.imuAHRSupdate(MotionVal[0] * 0.0175, MotionVal[1] * 0.0175,MotionVal[2] * 0.0175, MotionVal[3],MotionVal[4],MotionVal[5], MotionVal[6], MotionVal[7], MotionVal[8])
-    pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
-    roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
-    yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
-    mag   = math.atan2(Mag[1], Mag[0]) * 180/math.pi
+    # icm20948.icm20948CalAvgValue()
+    # icm20948.imuAHRSupdate(MotionVal[0] * 0.0175, MotionVal[1] * 0.0175,MotionVal[2] * 0.0175, MotionVal[3],MotionVal[4],MotionVal[5], MotionVal[6], MotionVal[7], MotionVal[8])
+    # pitch = math.asin(-2 * q1 * q3 + 2 * q0* q2)* 57.3
+    # roll  = math.atan2(2 * q2 * q3 + 2 * q0 * q1, -2 * q1 * q1 - 2 * q2* q2 + 1)* 57.3
+    # yaw   = math.atan2(-2 * q1 * q2 - 2 * q0 * q3, 2 * q2 * q2 + 2 * q3 * q3 - 1) * 57.3
+    # mag   = math.atan2(Mag[1], Mag[0]) * 180/math.pi
     # print("\r\n /-------------------------------------------------------------/ \r\n")
     # print('\r\n Roll = %.2f , Pitch = %.2f , Yaw = %.2f\r\n'%(roll,pitch,yaw))
     # print('\r\nAcceleration:  X = %d , Y = %d , Z = %d\r\n'%(Accel[0],Accel[1],Accel[2]))  
@@ -408,8 +407,9 @@ if __name__ == '__main__':
       "Mag0": Mag[0],
       "Mag1": Mag[1],
       "Mag2": Mag[2],
-      "Heading": mag
+      # "Heading": mag
     }
     data_json = json.dumps(data)
     r.publish('ICM20948_data', str(data_json))
+    time.sleep(0.5)
     
---
+++ tests/madgwick.test.js
@@ -0,0 +1,16 @@
+/* global describe, it */
+const madgwick = require('../lib/madgwick')
+const { expect } = require('chai')
+
+const data = { Accel0: 1000, Accel1: -797, Accel2: 14819, Gyro0: 1, Gyro1: 3, Gyro2: 0, Mag0: -37.25, Mag1: 489.25, Mag2: -542.875 }
+
+describe('The Madgwick the calculations', () => {
+  it('should take the input data and return the expected result', () => {
+    const result = madgwick(data.Gyro0, data.Gyro1, data.Gyro2, data.Accel0, data.Accel1, data.Accel2, data.Mag0, data.Mag1, data.Mag2)
+    const expected = { roll: -3.07, pitch: 3.83, yaw: -86.5 }
+
+    expect(result.roll).to.be.almost(expected.roll, 0.1)
+    expect(result.pitch).to.be.almost(expected.pitch, 0.1)
+    expect(result.heading).to.be.almost(expected.yaw, 0.1)
+  })
+})
