8d8c6ead251eb31d481e41a104e37b5f280789bd
Duncan Abraham <duncan@tydusolutions.co.uk>
2023-10-10 11:36:17 +0100

    test script

---
+++ trials/ups.js
@@ -0,0 +1,21 @@
+const UPS = require('../lib/i2c/UPS')  // Assuming UPS.js is in the same directory
+const log = require('../lib/log') // Change to actual path
+
+async function testUPS() {
+  const ups = new UPS() // You may need to provide options and additional arguments based on your constructor
+  
+  try {
+    console.log("Initializing...")
+    ups.init()
+
+    console.log("Reading Data...")
+    await ups.readData()
+
+    console.log("Data read:")
+    console.log(ups.data)
+  } catch (error) {
+    log.error("An error occurred:", error)
+  }
+}
+
+testUPS()
