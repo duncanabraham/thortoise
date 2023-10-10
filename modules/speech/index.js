f1639bee2c1a941f763e55c32dbf357ef5486032
Duncan Abraham <dunc@buzzler.co.uk>
2023-10-10 22:32:40 +0100

    sound engine

---
+++ modules/speech/index.js
@@ -0,0 +1,23 @@
+/**
+ * Speech reads text that has been published to the "voice" redis channel and says it out loud.
+ */
+
+const SpeechQueue = require('./lib/speechQueue')
+// eslint-disable-next-line no-unused-vars
+const speechQueue = new SpeechQueue() // speedhQueue is an event handler for the redis channel "voice"
+
+process.on('SIGINT', () => {
+  console.log('Received SIGINT. Cleaning up before exit.')
+  // Insert any cleanup code here
+  process.exit(0)
+})
+
+process.on('uncaughtException', (err) => {
+  console.error(`Uncaught Exception: ${err}`)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(`Unhandled Rejection at: Promise ${p}, reason: ${reason}`)
+  process.exit(1)
+})
---
+++ modules/speech/lib/speechQueue.js
@@ -0,0 +1,101 @@
+const redis = require('redis')
+const { exec } = require('child_process')
+
+const soundFiles = require('./sound.json')
+
+class SpeechQueue {
+  constructor (options = {}) {
+    this._queue = {}
+    this.currentlySpeaking = false
+    this.subscriber = redis.createClient()
+    this._initializeRedis()
+    this.maxAge = options.maxAge || 1000 * 60 * 3 // 3 minutes
+    this.audioDevice = options.audioDevice || '-Dhw:3'
+  }
+
+  _initializeRedis () {
+    this.subscriber.on('error', (err) => {
+      console.error(`Redis error: ${err}`)
+    })
+
+    this.subscriber.subscribe('voice')
+    this.subscriber.on('message', (channel, message) => {
+      this._addMessageToQueue(JSON.parse(message))
+    })
+  }
+
+  _validateAndSanitizeText (text) {
+    // Escape double quotes and backslashes to prevent command injection
+    const sanitizedText = text.replace(/(["\\])/g, '\\$1')
+
+    return sanitizedText
+  }
+
+  _handleCommand (command) {
+    switch (command) {
+      case 'CLEAR':
+        this.clearQueue()
+        break
+      case 'BEEP':
+        exec(`aplay ${soundFiles.BEEP}`, this._postSoundCommand.bind(this))
+        break
+      case 'BOOP':
+        exec(`aplay ${soundFiles.Boop}`, this._postSoundCommand.bind(this))
+        break
+      default:
+        console.error(`Unknown command: ${command}`)
+    }
+  }
+
+  _addMessageToQueue (voiceObject) {
+    const { text, timestamp, command } = voiceObject
+    if (command) {
+      this._handleCommand(command)
+      return
+    }
+    const sanitizedText = this._validateAndSanitizeText(text)
+    if (!sanitizedText) {
+      console.error('Invalid text received')
+      return
+    }
+    this._queue[timestamp] = sanitizedText
+    this._pruneOldMessages()
+    if (!this.currentlySpeaking) {
+      this._speak()
+    }
+  }
+
+  _pruneOldMessages () {
+    const old = Date.now() - this.maxAge
+    Object.keys(this._queue).forEach(key => {
+      if (key < old) {
+        delete this._queue[key]
+      }
+    })
+  }
+
+  _speak () {
+    if (Object.keys(this._queue).length === 0) {
+      return
+    }
+    this.currentlySpeaking = true
+    const oldestTimestamp = Math.min(...Object.keys(this._queue))
+    const textToSpeak = this._queue[oldestTimestamp]
+
+    delete this._queue[oldestTimestamp]
+    exec(`espeak "${textToSpeak}" --stdout | aplay ${this.audioDevice}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error in speaking: ${error}`)
+        return
+      }
+      this.currentlySpeaking = false
+      this._speak()
+    })
+  }
+
+  clearQueue () {
+    this._queue = {}
+  }
+}
+
+module.exports = SpeechQueue
